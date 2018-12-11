257e60bfe44d227a30b20bb721778892a89b71f4
DESKTOP-NNMFK6A\DoYoung <doyoung0205@naver.com>
2018-12-11 15:59:51 +0900

    CCC

--- BestianDental/target/classes/static/js/main.js
+++ BestianDental/target/classes/static/js/main.js
@@ -1,8 +1,6 @@
 	$(document).ready(function() {
-
 		
 		var menuBig = $('.sidebarMenu');
-
 		
 		$('#mobilemenu').on('click', function(e) {
 			/* 기본동작 중지코드 ex) A태그 */
@@ -59,20 +57,61 @@
 			});
 			
 		    $('.overlay92').fadeOut();
+		    
 		    $('html').css('overflow-y','auto');
+		    
 		});
 		
 		$(".headerDoor").mouseover(function(){
 			if(!$(".bannerBox").hasClass("active")){
 				$(".bannerBox").addClass("active");
 			}
-		})
+		});
 		
 		$(".headerDoor").mouseleave (function(){
 			if($(".bannerBox").hasClass("active")){
 				$(".bannerBox").removeClass("active");
 			}
-		})
+		});
+		
+		/*ul 태그*/
+        var $gnbList = $("ul.theory");
+        var $gnbMenu = $gnbList.find("> li");
+        /*span 태그*/
+        var $gnbAcc = $(".liUnder");
+        
+        /* a 태그*/
+        var menuGap = parseInt($gnbMenu.find("> a").css("paddingLeft"));
+            
+        $gnbMenu.on("mouseenter.hover focusin.hover", function(){
+            var gnbAccPos = $(this).offset().left - $(".banner-container").offset().left; 
+            var gnbWidth = $(this).find("a").width()*1;
+            
+            if( gnbAccPos === 0 ) {
+                TweenMax.to( $gnbAcc, .1, { x : menuGap,width:gnbWidth } );
+            } else {
+                TweenMax.to( $gnbAcc, .1, { x : gnbAccPos+menuGap,width:gnbWidth } );
+            }
+        });
+        
+        var $uberSection =$(".uberSection");
+        var $uberColumn =$uberSection.find(".uberColumn");
+        
+        $uberColumn.on("mouseenter.hover focusin.hover", function(){
+        	
+             var curIndex = $(this).index();
+             
+        	 var gnbAccPos = $gnbMenu.eq(curIndex).find("a").offset().left - $(".banner-container").offset().left; 
+        	 var gnbWidth = $gnbMenu.eq(curIndex).find("a").width()*1;
+        	
+        	if( gnbAccPos === 0 ) {
+        		TweenMax.to( $gnbAcc, .1, { x : menuGap,width:gnbWidth } );
+        	} else {
+        		TweenMax.to( $gnbAcc, .1, { x : gnbAccPos+menuGap,width:gnbWidth } );
+        	}
+        	
+        });
+		
 	});
 	 
 	$(window).resize(function() {
@@ -80,6 +119,6 @@
 
 		$("section.wideBanner").css({
 			width : widthMove
-		})
+		});
 
 	});
